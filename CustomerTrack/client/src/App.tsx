6b3be08bb712a44d4ccb4d4b80febfdcf8e69330
lovelysehotra <lsehotra12@gmail.com>
2025-01-29 23:43:15 +0530

    setup svelte app for practice

--- CustomerTrack/client/src/App.tsx
+++ CustomerTrack/client/src/App.tsx
@@ -1,29 +1,31 @@
-import React, { useEffect, useState } from "react";
-import CustomerForm from "./Components/CustomerForm/CustomerForm";
-import CustomerList from "./Components/CustomerList/CustomerList";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect,  } from "react";
+// import CustomerForm from "./Components/CustomerForm/CustomerForm";
+// import CustomerList from "./Components/CustomerList/CustomerList";
+import { useDispatch,  } from "react-redux";
 import { getAllCustomers } from "./Redux/Slices/CustomerSlice";
+import Drag from "../src/Components/Drag"
 
 const App: React.FC = () => {
   const dispatch = useDispatch()
-  const data = useSelector((state)=> state?.customer)
+  // const data = useSelector((state)=> state?.customer)
   useEffect(() => {
     //@ts-ignore
     dispatch(getAllCustomers());
   }, [dispatch]);
   return (
     <div className="App">
-      <h1>Customer Management</h1>
+      {/* <h1>Customer Management</h1>
 
-      {/* Customer Form */}
+      
       <CustomerForm />
       {data.customers && data.customers.length > 0 ? 
   data.customers.map((el: any) => (
-    // Return the JSX from map
+    
     <CustomerList key={el._id} customers={{...el}} />
   )) 
   : <h1>No Customers</h1>
-}
+} */}
+<Drag/>
     </div>
   );
 };
